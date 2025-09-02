69b6b86a510a2407defa39579bbcafdf99747fbe
Nadav Shamir <nadav7901@gmail.com>
2025-09-02 12:19:52 +0300

    Add error message css

--- GTMFrontend/src/App.jsx
+++ GTMFrontend/src/App.jsx
@@ -1,26 +1,38 @@
 import { useState } from 'react'
 import { CustomInput, CustomButton } from './components'
+import { isValidIPv4 } from './utils/validation'
 import './App.css'
 
 // Main App Component
 function App() {
   const [inputValue, setInputValue] = useState('')
   const [location, setLocation] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value)
   }
 
-  const fetchLocationFromIP = async () => {
+  const handleSubmit = async () => {
+    // reset error message and location
+    setErrorMessage('')
+    setLocation('')
+
+    if (!isValidIPv4(inputValue)) {
+      setErrorMessage('Please enter a valid IPv4 address')
+      return
+    }
+    
     const response = await fetch(`${import.meta.env.VITE_SERVER_IP}:${import.meta.env.VITE_SERVER_PORT}/v1/find-country?ip=${inputValue}`)
     const data = await response.json()
 
     if (!response.ok) {
+      // handle errors from server
       const errorMessage = data.error
-      setLocation(errorMessage)
+      setErrorMessage(errorMessage)
     }
     else {
-      const parsedLocation = `Location: ${data.country}, ${data.city}`
+      const parsedLocation = `${data.country}, ${data.city}`
       setLocation(parsedLocation)
     }
   }
@@ -36,20 +48,29 @@ function App() {
           <CustomInput
             value={inputValue}
             onChange={handleInputChange}
-            placeholder="Enter IP address here..."
+            placeholder="Enter IPv4 address (e.g., 8.8.8.8)"
             className="text-input"
           />
           <CustomButton
-            onClick={fetchLocationFromIP}
+            onClick={handleSubmit}
             className="submit-button"
           >
-            Submit
+            Find
           </CustomButton>
         </div>
+        
+        {errorMessage && (
+          <div className="error-display">
+            <p>{errorMessage}</p>
+          </div>
+        )}
+        
+        {location && (
+          <div className="location-display">
+            <p>{location}</p>
+          </div>
+        )}
       </main>
-      <div className="location-display">
-          <p>{location}</p>
-        </div>
     </div>
   )
 }
